804fff7e76e047a650812bee8f3af7cf7907d212
Hossein Salehi <hossein@sisuwellness.com>
2020-02-21 17:07:53 +1100

    SU-3111 SU-3139 Cleanup.

--- src/qrisk3.js
+++ src/qrisk3.js
@@ -1,6 +1,16 @@
 import qrisk from "./lib";
 import inputBuilder from "./inputs";
 
+const Disclaimer = `  
+    The initial version of this file, to be found at http://svn.clinrisk.co.uk/opensource/qrisk2, faithfully implements QRISK3-2017.
+    ClinRisk Ltd. have released this code under the GNU Lesser General Public License to enable others to implement the algorithm faithfully.
+    However, the nature of the GNU Lesser General Public License is such that we cannot prevent, for example, someone accidentally 
+    altering the coefficients, getting the inputs wrong, or just poor programming.
+    ClinRisk Ltd. stress, therefore, that it is the responsibility of the end user to check that the source that they receive produces the same 
+    results as the original code found at https://qrisk.org.
+    Inaccurate implementations of risk scores can lead to wrong patients being given the wrong treatment. 
+`;
+
 const calculateScore = qriskInput => {
     const sex = qriskInput.sex;
     let score = -1;
@@ -65,4 +75,4 @@ const calculateScore = qriskInput => {
     return score;
 };
 
-export { calculateScore, inputBuilder };
+export { calculateScore, inputBuilder, Disclaimer };
