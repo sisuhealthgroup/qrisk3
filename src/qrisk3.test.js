20eb6b5e543b4f5d0786423aad97d35157a3a737
Hossein Salehi <hossein@sisuwellness.com>
2020-03-16 09:35:00 +1100

    SU-3244 Add test for age, sex, and chdl variations.

--- src/qrisk3.test.js
+++ src/qrisk3.test.js
@@ -1,4 +1,5 @@
 import { calculateScore, inputBuilder } from "./qrisk3";
+const { Sex, Ethnicity, DiabetesStatus, SmokingStatus } = inputBuilder;
 
 describe("QRISK-3 tests", () => {
     it("calculateScore() Should throw when qrisk input is not defined ", () => {
@@ -6,7 +7,6 @@ describe("QRISK-3 tests", () => {
     });
 
     it("calculateScore() Should correctly calculate the qrisk score for a typical female ", () => {
-        const { Sex, Ethnicity, DiabetesStatus, SmokingStatus } = inputBuilder;
         const aboutYou = inputBuilder.buildAboutYou(75, Sex.female, Ethnicity.white);
         const clinical = inputBuilder.buildClinical(
             SmokingStatus.nonSmoker,
@@ -32,7 +32,6 @@ describe("QRISK-3 tests", () => {
     });
 
     it("calculateScore() Should correctly calculate the qrisk score for a typical male ", () => {
-        const { Sex, Ethnicity, DiabetesStatus, SmokingStatus } = inputBuilder;
         const aboutYou = inputBuilder.buildAboutYou(75, Sex.male, Ethnicity.white);
         const clinical = inputBuilder.buildClinical(
             SmokingStatus.nonSmoker,
@@ -56,4 +55,44 @@ describe("QRISK-3 tests", () => {
 
         expect(score).toBeCloseTo(24.59, 2);
     });
+
+    const inputs = [
+        [75, Sex.male, Ethnicity.white, 4, 24.6],
+        [65, Sex.male, Ethnicity.white, 4, 12.7],
+        [55, Sex.male, Ethnicity.white, 8, 11.7],
+        [35, Sex.male, Ethnicity.white, 3.5, 0.6],
+
+        [75, Sex.female, Ethnicity.white, 4, 18.1],
+        [65, Sex.female, Ethnicity.white, 4, 8.0],
+        [55, Sex.female, Ethnicity.white, 8, 6.1],
+        [35, Sex.female, Ethnicity.white, 3.5, 0.4]
+    ];
+    it.each(inputs)(
+        "calculateScore(): [sys:120, sysVar:10, height:170, weight:70, ethnicity:white, townsendSCore:0]\-" +
+        "--[age:%p, sex:%p, ethnicity:%p, chdl:%p] ==> qriskScore:%p",
+        (age, sex, ethnicity, chdl, expectedScore) => {
+            const aboutYou = inputBuilder.buildAboutYou(age, sex, ethnicity);
+            const clinical = inputBuilder.buildClinical(
+                SmokingStatus.nonSmoker,
+                DiabetesStatus.none,
+                false,
+                false,
+                false,
+                false,
+                false,
+                false,
+                false,
+                false,
+                false,
+                false
+            );
+            const bmi = 70 / (1.7 * 1.7);
+            const biometric = inputBuilder.buildBiometrics(chdl, 120, 10, bmi);
+            const townsendScore = 0;
+            const qriskInput = inputBuilder.buildQriskInput(aboutYou, clinical, biometric, townsendScore);
+            const score = calculateScore(qriskInput);
+
+            expect(parseFloat(score.toFixed(1))).toBeCloseTo(expectedScore, 1);
+        }
+    );
 });
